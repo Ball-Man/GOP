6e53706738c94124e51f7c8b6290a81baa22d43f
Ball-Man <francesco.mistri98@gmail.com>
2018-02-10 03:06:17 +0100

    Cards and squares
    Added headers containing 2 enums(Cards and Squares) which define which type of cards and squares are going to be included into the project. The specific classes are future implementation.

---
+++ include/cards.h
@@ -0,0 +1,17 @@
+#ifndef GOP_CARDS_H_
+#define GOP_CARDS_H_
+
+// Different types of cards
+enum class Cards
+{
+  NOTHING,
+  COINS,
+  LOSE_COINS,
+  BUY_FORWARD,
+  WORST_FORWARD,
+  WORST_BACK,
+  BEST_FORWARD,
+  EVERYONE_TO_ZERO,
+};
+
+#endif
---
+++ include/squares.h
@@ -0,0 +1,16 @@
+#ifndef GOP_SQUARES_H_
+#define GOP_SQUARES_H_
+
+// Different types of squares
+enum class Squares
+{
+  EMPTY,
+  FORWARD,
+  BACK,
+  DRAW,
+  STOP,
+  COINS,
+  REROLL,
+};
+
+#endif
