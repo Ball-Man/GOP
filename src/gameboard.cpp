6384d90c894a434eda05beb582e4b738a4c2423a
Ball-Man <francesco.mistri98@gmail.com>
2018-02-10 03:05:45 +0100

    Gameboard destructor
    Forgot about it. Wops.

--- src/gameboard.cpp
+++ src/gameboard.cpp
@@ -7,6 +7,13 @@ Gameboard::Gameboard(Vector<Square*>& squares)
   player_ = 0;
 }
 
+Gameboard::~Gameboard()
+{
+  for(int i = 0; i < squares_.Length(); i++)
+    if(squares_[i])   // Safety is number one priority
+      delete squares_[i];
+}   
+
 // Getters / Setters
 Deck& Gameboard::Cards() { return deck_; }
 
